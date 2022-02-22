002b6fbf2966e7a0deb722170e8d5b8c3023bfa7
ccqueen <chobitsnerv@gmail.com>
2022-02-22 23:24:32 +0900

    修音音频的弹窗解释页面信息更新

--- src/components/popup/ExplainTreated.vue
+++ src/components/popup/ExplainTreated.vue
@@ -15,17 +15,12 @@ const emit = defineEmits(["closepopup"]);
   <MainPopUp v-on:closepopup="$emit('closepopup')" title="经处理的歌">
     <div class="content">
       <p>
-        经处理的歌是指将录播片段消除了说话的部分，但是音质可能有部分损失。仅有少部分歌曲经过处理，其他歌曲仍播放未处理版本。
+        经处理的歌是指将录播片段进行了修音，虽然和原版相比可能部分音质有所损失。
+      </p>
+      <p>
+        仅有少部分歌曲经过处理，其他歌曲仍播放未处理版本。欢迎大家投递自己的精修版本。
       </p>
       <p>通常情况下，网站提供的音频是对录播音频进行简单剪辑，不做额外处理。</p>
-      <p>
-        这个处理过程是由<a
-          href="https://space.bilibili.com/545133496"
-          target="_blank"
-          rel="noreferrer noopener"
-          >@枺芙</a
-        >完成的。
-      </p>
     </div>
   </MainPopUp>
 </template>
