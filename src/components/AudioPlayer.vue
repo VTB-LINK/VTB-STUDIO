e08adae7ce138c8c83327514c427abd7f7875638
ccqueen <chobitsnerv@gmail.com>
2022-03-09 01:33:42 +0900

    fix issue of playing song info will be replaced after changing the playlist order

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -35,6 +35,7 @@ const playProgress = ref(0);
 const loadProgress = ref(0);
 const duration = ref(0);
 const currentSongIndex = ref(0);
+const currentSongID = ref("");
 const playlist = ref(window.AudioLists.playlist);
 const loveList = ref(window.AudioLists.love_list);
 const volumebarref = ref(null);
@@ -217,6 +218,7 @@ const applySong = async () => {
     });
   }
   audio.title = currentSongObject.value.name;
+  currentSongID.value = currentSongObject.value.id;
   // 保存当前歌单
   //utils.savePlaylist(currentSongIndex.value, playlist.value);
 };
@@ -442,6 +444,14 @@ const playlistScroll = () => {
   ].scrollIntoView({
     block: "nearest",
   });
+};
+
+const updateCurrentSongIndex = (evt) => {
+  const _actualCurrentSongIndex = playlist.value.findIndex(
+    (song) => song.id === currentSongID.value
+  );
+  if (_actualCurrentSongIndex !== currentSongIndex.value)
+    currentSongIndex.value = _actualCurrentSongIndex;
 };
 
 onMounted(() => {
@@ -750,6 +760,7 @@ defineExpose({
               name: 'flip-list',
               type: 'transition',
             }"
+            @change="updateCurrentSongIndex"
           >
             <template #item="{ element, index }">
               <div
