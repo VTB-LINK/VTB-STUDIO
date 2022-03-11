b7b250e27cc05e187af3382d8932df4bb7ac0826
ccqueen <chobitsnerv@gmail.com>
2022-03-11 23:04:38 +0900

    change to tua-body-scroll-lock

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -19,6 +19,7 @@ import bus from "vue3-eventbus";
 import draggable from "vuedraggable";
 import SharePopUp from "popup/Share.vue";
 import DetailsPopUp from "popup/Details.vue";
+import { lock, unlock } from "tua-body-scroll-lock";
 
 let audio = {};
 let audioSource = {};
@@ -471,21 +472,23 @@ const updateCurrentSongIndex = (evt) => {
 let scrollPosition = 0;
 // 弹出框出现时锁定背景防止滚动穿透
 const lockBackGroundForPopup = () => {
-  const $body = document.querySelector("body");
-  scrollPosition = window.pageYOffset;
-  $body.style.overflow = "hidden";
-  $body.style.position = "fixed";
-  $body.style.top = `-${scrollPosition}px`;
-  $body.style.width = "100%";
+  // const $body = document.querySelector("body");
+  // scrollPosition = window.pageYOffset;
+  // $body.style.overflow = "hidden";
+  // $body.style.position = "fixed";
+  // $body.style.top = `-${scrollPosition}px`;
+  // $body.style.width = "100%";
+  lock(playlistcontentref.value);
 };
 // 关闭是解除背景锁定
 const unlockBackGroundForPopup = () => {
-  const $body = document.querySelector("body");
-  $body.style.removeProperty("overflow");
-  $body.style.removeProperty("position");
-  $body.style.removeProperty("top");
-  $body.style.removeProperty("width");
-  window.scrollTo(0, scrollPosition);
+  // const $body = document.querySelector("body");
+  // $body.style.removeProperty("overflow");
+  // $body.style.removeProperty("position");
+  // $body.style.removeProperty("top");
+  // $body.style.removeProperty("width");
+  // window.scrollTo(0, scrollPosition);
+  unlock(playlistcontentref.value);
 };
 
 onMounted(() => {
